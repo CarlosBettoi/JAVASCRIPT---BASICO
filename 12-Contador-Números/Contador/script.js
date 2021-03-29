0eecc1cb3e2af6879333c802d558cd5e7415d398
Carlos Cavalcanti <Carlos Alberto Bettoi Cavalcanti>
2021-03-29 13:26:39 -0300

    Contando Números

---
+++ 12-Contador-Números/Contador/script.js
@@ -0,0 +1,36 @@
+function contar () {
+    var ini = document.getElementById ('txti')
+    var fim = document.getElementById ('txtf')
+    var pas = document.getElementById ('txtp')
+    var res = document.getElementById ('res')
+    var i = Number (ini.value)
+    var f = Number (fim.value)
+    var p = Number (pas.value)
+    if (ini.value.length == 0 || fim.value.length == 0 ) {
+        res.innerHTML = 'impossível contar'
+        
+   
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        if (p <= 0) {
+            window.alert ('Passo = 1')
+            p = 1 
+        }
+        if ( i < f) {
+            for (var c = i; c <= f; c+=p) {
+            res.innerHTML += `${c} \u{1F449} `
+            }
+       
+        } else {
+            for (var c = i; c >= f; c-=p) {
+                res.innerHTML += `${c} \u{1f449}`
+            }
+        }    
+    }
+    res.innerHTML += `\u{1F3C1}`
+
+ }
+
+   
+  
+
